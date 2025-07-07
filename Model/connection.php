d966ba535837900be050184c21d295a5e766353c
bilal raza <huzaifa2582003@gmail.com>
2025-07-08 01:00:25 +0500

    new

--- Model/connection.php
+++ Model/connection.php
@@ -1,8 +1,16 @@
 <?php
-$db="able-choice";
-$host="localhost";
-$user_name="root";
-$password="";
+if($_SERVER['SERVER_NAME'] == 'localhost'){
+    $db="able-choice";
+    $host="localhost";
+    $user_name="root";
+    $password="";
+}else{
+    $db="cyberns2_able_choice";
+    $host="localhost";
+    $user_name="cyberns2_able_choice_user";
+    $password="b2582003@";
+
+}
 
 $con=mysqli_connect($host,$user_name,$password,$db) or die("Something went wrong! Connection Failed!");
 
