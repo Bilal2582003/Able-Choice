0d88516ecc3dc03c641f2dcc53acbf2c49576253
bilal raza <huzaifa2582003@gmail.com>
2025-07-10 21:29:29 +0500

    new

--- Views/Index.php
+++ Views/Index.php
@@ -71,7 +71,7 @@ setToken($con);
       <div class="container">
         <div class="row justify-content-center">
           <div class="col-lg-12 text-center " style="min-height:fit-content !important">
-            <h2 data-aos="fade-down">Welcome to <span>Able Coice</span></h2>
+            <h2 data-aos="fade-down">Welcome to <span>Able Choice</span></h2>
             <p data-aos="fade-up">Our Company name is <strong>Al-Sheikh Enterprise</strong>. We are providing our service till 25 years. We have <b>one of best quality products</b>.</p>
             <a data-aos="fade-up" data-aos-delay="200" href="Product.php" class="btn-get-started">See Our Products</a>
           </div>
