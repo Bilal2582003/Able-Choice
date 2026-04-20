0713f0bbb25da98c8cc756656a534af608a4607e
huzaifa2582003 <huzaifa2582003@gamil.com>
2026-04-20 17:23:00 +0500

    new

--- Views/Master/breadcrumbHeader.php
+++ Views/Master/breadcrumbHeader.php
@@ -6,7 +6,7 @@
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
   <title><?php echo $page?></title>
-  <meta content="Able choice, able choice, able, choice, Able-choice, marble , marbel, fuirt, dish, kitchen, room, decorate, massage" name="description">
+  <meta content="High-quality handmade marble handicrafts from Pakistan. Exporter of marble plates, bowls, vases & custom items. Contact Al-Shaikh Enterprises, Able choice, able choice, able, choice, Able-choice, marble , marbel, fuirt, dish, kitchen, room, decorate, massage" name="description">
   <meta content="Able choice, able choice, able, choice, Able-choice, marble , marbel, fuirt, dish, kitchen, room, decorate, massage" name="keywords">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
